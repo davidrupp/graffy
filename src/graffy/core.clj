f1bce1eb02e30c1effb52b968b6e8c296f08a673
David Rupp <david@ruppworks.com>
2013-03-09 09:01:37 -0800

    Added add-nodes function.

--- src/graffy/core.clj
+++ src/graffy/core.clj
@@ -5,15 +5,18 @@
     (swap! g assoc n #{}))
   @g)
 
+(defn add-nodes [g & ns]
+  (doseq [n ns]
+    (add-node g n)))
+
 (defn add-edge [g s d]
-  (add-node g s)
-  (add-node g d)
+  (add-nodes g s d)
   (swap! g update-in [s] conj d)
   (swap! g update-in [d] conj s)
   @g)
 
 (comment
   (def g (atom {}))
-  (add-node g :a)
   (add-edge g :a :b)
+  (add-edge g :a :c)
 )
